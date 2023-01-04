4174538dd7eb7f6e1e7a7c157757e3553feaa31b
TimothyCruickshank <111780723+TimothyCruickshank@users.noreply.github.com>
2023-01-04 12:08:36 -0500

    first commit

---
+++ API.py
@@ -0,0 +1,9 @@
+import requests
+
+response = requests.get("https://api.thedogapi.com/")
+request = response.request
+
+print(response.status_code)
+print(response.text)
+print(response.headers)
+print(request.headers)
